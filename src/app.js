06d45bfce9a92d570a1f667a213e6f1f001ef609
sergiopereiraj <sergiopereira.jorquera@gmail.com>
2021-11-12 19:25:29 +0000

    guardado

--- src/app.js
+++ src/app.js
@@ -7,25 +7,39 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  const value = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const suits = ["♦", "♥", "♠", "♣"];
+  const suitUp = document.querySelector(".suit-up");
+  const suitDown = document.querySelector(".suit-down");
+  const pinta = suits[Math.floor(Math.random() * 4)];
+  const p1 = document.createElement("p");
+  p1.classList.add("text-danger");
+  p1.innerHTML = pinta;
+  const p2 = document.createElement("p");
+  p2.classList.add("text-danger");
+  p2.innerHTML = pinta;
+  suitUp.appendChild(p1);
+  suitDown.appendChild(p2);
 
-  const game = () => {
-    const value = [
-      "A",
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "J",
-      "Q",
-      "K"
-    ];
-    //const suits = ["<i class="fas fa-heart text-danger"></i>", "diamond", "spade", "club"];
-    function constructor() {}
-  };
+  const number = document.querySelector(".number");
+  const numero = value[Math.floor(Math.random() * 14)];
+  const p = document.createElement("p");
+  p.classList.add("text-success");
+  p.innerHTML = numero;
+  number.appendChild(p);
 };
