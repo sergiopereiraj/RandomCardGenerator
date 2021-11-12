f5b277f81ac79e8be8e12064be2db9f1ff077d9f
sergiopereiraj <sergiopereira.jorquera@gmail.com>
2021-11-12 15:04:04 +0000

    guardado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const game = () => {
+    const value = [
+      "A",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    //const suits = ["<i class="fas fa-heart text-danger"></i>", "diamond", "spade", "club"];
+  };
 };
